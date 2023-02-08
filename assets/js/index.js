9b098f5f6358fe7e144a3aa4c363a9f04301d4dd
Mohammad Rahimi <moyi.pary@gmail.com>
2023-02-09 02:13:48 +0330

    feature/refactor-code
    1- add a class for side_menu to work easier with it
    2- change style, when side_menu is open
    3- decided to initail the side_menu in index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Menu button
-    let menu_btn = document.querySelector(".header__menu-btn");
-    let menu_btn__burger = document.querySelector(".header__menu-btn__burger");
-    let side_menu = document.querySelector(".side-menu");
-
-    menu_btn.addEventListener("click", function () {
-        menu_btn__burger.classList.toggle("open");
-        if (menu_btn__burger.classList.contains("open")) {
-            side_menu.classList.add("open");
-            document.body.classList.add('disabled');
-            document.body.querySelector('.main').classList.add('disabled');
-        }
-        else {
-            side_menu.classList.remove("open");
-            document.body.classList.remove('disabled');
-            document.body.querySelector('.main').classList.remove('disabled');
-        }
-
-    });
     // Menu button
 
+
     // Tabs
-    let tab_buttons = document.querySelectorAll('.side-menu__btn-group button');
+    let tab_buttons = document.querySelectorAll(".side-menu__btn-group button");
     tab_buttons.forEach(function (btn) {
         btn.addEventListener("click", function () {
             tab_buttons.forEach(function (b) {
-                b.classList.remove('active');
+                b.classList.remove("active");
                 let tab = document.querySelector(b.dataset.bsTarget);
-                tab.classList.remove('show');
-                tab.classList.remove('active');
+                tab.classList.remove("show");
+                tab.classList.remove("active");
             });
 
-            btn.classList.toggle('active');
+            btn.classList.toggle("active");
             let tab_panel = document.querySelector(btn.dataset.bsTarget);
             tab_panel.classList.toggle("show");
             tab_panel.classList.toggle("active");
